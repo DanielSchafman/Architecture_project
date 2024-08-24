3084dc790f1f7962e244cdd84ee6ff6bbcbb9d4d
danielS <daniel.schafman@gmail.com>
2024-08-24 18:47:14 +0300

    created .sql file

---
+++ mysql.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS Lines;
+
+CREATE TABLE Lines (
+    ID CHAR(36) PRIMARY KEY,
+    ObjectPath VARCHAR(255) NOT NULL,
+    Date VARCHAR(50) NOT NULL,
+    AmountOfLines INT NOT NULL
+);
+
+
+DROP TABLE IF EXISTS Words;
+
+CREATE TABLE Words(
+    ObjectPath VARCHAR(255) NOT NULL,
+    Date VARCHAR(50) NOT NULL,
+    AmountOfWords INT NOT NULL,
+    PRIMARY KEY (ObjectPath,Date)
+);

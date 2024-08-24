a5da62ae5710aef622cc80934b1410e2c16f404a
danielS <daniel.schafman@gmail.com>
2024-08-24 19:48:55 +0300

    updated terraform file

--- DATABASE/creatingDB.tf
+++ DATABASE/creatingDB.tf
@@ -2,28 +2,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "rds_endpoint" {
+  description = "The endpoint for the MySQL RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The password for the MySQL database"
+  type        = string
+  sensitive   = true
+}
 
 resource "aws_instance" "mysql-client" {
-    ami = "ami-0e86e20dae9224db8"
-    instance_type = "t2.micro"
-    key_name = "KEY_NAME"
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+  key_name      = "beanstalk-key"
 
-    provisioner "remote-exec" {
-        inline = [ 
-            "sudo apt-get update -y",
-            "sudo apt-get install -y mysql-client git",
-            "mkdir -p /var/tmp/architecure_project",
-            "cd /var/tmp/architecure_project",
-            "git clone https://github.com/DanielSchafman/Architecture_project.git",
-            "mysql -h ${aws_db_instance.mudb.endpoint} -p 3306 -u admin -p ${yourDBPassword} < mysql.sql"
-         ]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y mysql-client git",
+      "mkdir -p /var/tmp/architecture_project",
+      "cd /var/tmp/architecture_project",
+      "git clone https://github.com/DanielSchafman/Architecture_project.git",
+      "mysql -h ${var.rds_endpoint} -P 3306 -u admin -p${var.db_password} < mysql.sql"
+    ]
 
-
-         connection {
-           type = "ssh"
-           user = "ubuntu"
-           private_key = file("~/.ssh/PRIVATE_KEY")
-           host = self.public_ip
-         }
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("/home/daniel/Downloads/beanstalk-key.pem") #Change to your private key absolute path
+      host        = self.public_ip
     }
+  }
 }
